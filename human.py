e6fc9cf3fa079aa4b24f6a195cfa05149856a4ea
rashad porter <rashad.porter18@gmail.com>
2022-03-07 15:43:42 -0500

    test

---
+++ human.py
@@ -0,0 +1,5 @@
+from player import Player
+
+
+class Human(Player):
+    def __init__(self):
--- player.py
+++ player.py
@@ -2,5 +2,6 @@
 
 class Player:
     def __init__(self):
-        self.gestures = ['rock, paper, scissors, lizard, spock']
-
+        self.gestures = [2]
+        
+# ['rock, paper, scissors, lizard, spock']
